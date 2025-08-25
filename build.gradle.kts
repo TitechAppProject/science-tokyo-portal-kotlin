6cc5dbf8acbcdd3b9e027a7c62490e1184987289
nanashiki <nanashiki.app@gmail.com>
2025-08-25 12:04:20 +0900

    ver. 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "app.titech"
             artifactId = "science-tokyo-portal"
-            version = "1.0.0"
+            version = "1.1.0"
 
             from(components["java"])
 
